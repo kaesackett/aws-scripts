5235cd295c0700dcc89233bb74e957d23e6ba9b9
Kaelyn Sackett <kaesackett@yahoo.com>
2016-07-20 13:54:05 -0700

    Updated to affect all US regions
    Script now applies to all US regions rather than just us-east-1

--- autosnapshot_rds_instances.py
+++ autosnapshot_rds_instances.py
@@ -9,39 +9,53 @@ import re
 import boto3
 import datetime
 
-def snapshot_all_rds_instances():
+def get_regions():
+    ec2 = boto3.client('ec2', 'us-east-1')
+    resp = ec2.describe_regions(Filters=[{'Name': 'region-name', 'Values': [ '*us*' ]}])
+
+    regions = []
+
+    for endpoint in resp['Regions']:
+        regions.append(endpoint['RegionName'])
+
+    return regions
+
+def snapshot_all_rds_instances(regions):
     """Creates logical snapshots of all RDS instances. Nomenclature: 'auto-<YYYY-MM-DD>-<RDS Instance Identifier>'"""
 
-    client = boto3.client('rds', region_name='us-east-1')
-    instances = client.describe_db_instances()
+    for region in regions:
+        client = boto3.client('rds', region_name=region)
+        instances = client.describe_db_instances()
+    
+        for instance in instances['DBInstances']:
+            response = client.create_db_snapshot(
+            DBSnapshotIdentifier='auto-{0}-{1}'.format(str(datetime.date.today()), instance['DBInstanceIdentifier']),
+            DBInstanceIdentifier=instance['DBInstanceIdentifier'],
+            Tags=[
+                    {'Key': 'Description',
+                    'Value': 'Automated DB Snapshot for RDS instance: {0}'.format(instance['DBInstanceIdentifier'])},
+                ]
+            )
 
-    for instance in instances['DBInstances']:
-        response = client.create_db_snapshot(
-        DBSnapshotIdentifier='auto-{0}-{1}'.format(str(datetime.date.today()), instance['DBInstanceIdentifier']),
-        DBInstanceIdentifier=instance['DBInstanceIdentifier'],
-        Tags=[
-                {'Key': 'Description',
-                'Value': 'Automated DB Snapshot for RDS instance: {0}'.format(instance['DBInstanceIdentifier'])},
-            ]
-        )
-
-def delete_old_snapshots():
+def delete_old_snapshots(regions):
     """Everytime the script runs, clean out the snapshots it created last time it ran."""
 
-    client = boto3.client('rds', region_name='us-east-1')
-    snapshots = client.describe_db_snapshots()
+    for region in regions:
+        client = boto3.client('rds', region_name='us-east-1')
+        snapshots = client.describe_db_snapshots()
 
-    # Affect only the snapshots that were created by this script
-    pattern = re.compile("^auto-[0-9]{4}-([0][0-9]|[1][0-2])-([0][0-9]|[1][0-9]|[2][0-9]|[3][0-1])-[a-zA-Z0-9_-]*$")
+        # Affect only the snapshots that were created by this script
+        pattern = re.compile("^auto-[0-9]{4}-([0][0-9]|[1][0-2])-([0][0-9]|[1][0-9]|[2][0-9]|[3][0-1])-[a-zA-Z0-9_-]*$")
 
-    for snapshot in snapshots['DBSnapshots']:
-        if pattern.match(snapshot['DBSnapshotIdentifier']):
-            if snapshot['SnapshotCreateTime'].date() <= datetime.date.today() - datetime.timedelta(days=7):
-                response = client.delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
+        for snapshot in snapshots['DBSnapshots']:
+            if pattern.match(snapshot['DBSnapshotIdentifier']):
+                if snapshot['SnapshotCreateTime'].date() <= datetime.date.today() - datetime.timedelta(days=7):
+                    response = client.delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
 
 if __name__ == "__main__":
     try:
-        snapshot_all_rds_instances()
-        delete_old_snapshots()
+        regions = get_regions()
+        snapshot_all_rds_instances(regions)
+        delete_old_snapshots(regions)
     except Exception as err:
         print(err)
